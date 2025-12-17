4bd479f9edd3f1b07cc452fe1952e11732352c81
aaronfinnila <aaronfinnilaa@gmail.com>
2025-12-17 15:46:10 +0200

    project start

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+    <body className="bg-gray-500 min-h-screen">
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-white flex items-center justify-center">
+          Hello there!
+        </div>
+      </div>
+    </body>
+    </>
+  )
+}
+
+export default App
